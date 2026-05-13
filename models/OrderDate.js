c1d4d51198072442ca664285c0fd591fa65c7a5d
Wakil Israk Abir <wakilisrakabir@gmail.com>
2026-05-13 09:59:54 +0600

    back to privious step

--- models/OrderDate.js
+++ models/OrderDate.js
@@ -5,9 +5,7 @@ const orderDateSchema = new mongoose.Schema({
     knitting: { type: Array, default: [] },
     dyeing: { type: Array, default: [] },
     finishing: { type: Array, default: [] },
-    delivery: { type: Array, default: [] },
-    // 🟢 Status field add kora holo
-    orderStatus: { type: String, default: 'Active' }
-}, { timestamps: true }); // timestamps add kora holo jeno date onujayi sajano jay
+    delivery: { type: Array, default: [] }
+});
 
 module.exports = mongoose.model('OrderDate', orderDateSchema);
