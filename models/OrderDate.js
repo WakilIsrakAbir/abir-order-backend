8bfae426168316f4ec9c0ccb631b277533e35d13
Wakil Israk Abir <wakilisrakabir@gmail.com>
2026-05-13 16:41:35 +0600

    II

--- models/OrderDate.js
+++ models/OrderDate.js
@@ -5,7 +5,9 @@ const orderDateSchema = new mongoose.Schema({
     knitting: { type: Array, default: [] },
     dyeing: { type: Array, default: [] },
     finishing: { type: Array, default: [] },
-    delivery: { type: Array, default: [] }
-});
-
+    delivery: { type: Array, default: [] },
+    // 🟢 NEW: Status and Date
+    orderStatus: { type: String, default: 'On Process' },
+    completedDate: { type: Date }
+}, { timestamps: true });
 module.exports = mongoose.model('OrderDate', orderDateSchema);
