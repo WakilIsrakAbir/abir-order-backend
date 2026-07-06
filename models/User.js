8a9a015d1dec7c56f0eb9b9fa0834220f8e84710
Wakil Israk Abir <wakilisrakabir@gmail.com>
2026-07-06 11:14:39 +0600

    approver

--- models/User.js
+++ models/User.js
@@ -13,7 +13,7 @@ const userSchema = new mongoose.Schema({
     },
     role: {
         type: String,
-        enum: ['Admin', 'Planner', 'Viewer'], 
+        enum: ['Admin', 'Approver', 'Planner', 'Viewer'], 
         default: 'Viewer'
     },
     plainPassword: {
